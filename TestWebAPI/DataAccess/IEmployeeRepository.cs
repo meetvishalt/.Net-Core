6f7c3414b411a54874decfb412aaac5a4d29d734
meetvishalt <meet_vishalt@yahoo.co.in>
2019-06-17 14:54:48 +0000

    changes for migration

---
+++ TestWebAPI/DataAccess/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployee(int id);
+
+        IEnumerable<Employee> GetAllEmployees();
+
+        void AddEmployee(Employee emp);
+
+        void UpdateEmployee(Employee emp);
+
+        void DeleteEmployee(int id);
+    }
+}
---
+++ TestWebAPI/DataAccess/MockEmployeeRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() {Id = 1, Name = "Mary", Department = "HR", Email = "mary@citiustech.com"},
+                new Employee() {Id = 2, Name = "Viajy", Department = "HMT", Email = "vijay@citiustech.com"},
+                new Employee() {Id = 3, Name = "Prerana", Department = "IT", Email = "prerana@citiustech.com"}
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == id);
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+            _employeeList.Add(emp);
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            var employee = _employeeList.FirstOrDefault(e => e.Id == emp.Id);
+            
+            if (employee != null)
+            {
+                employee = emp;
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var employee = _employeeList.FirstOrDefault(e => e.Id == id);
+
+            if (employee != null)
+            {
+                _employeeList.Remove(employee);
+            }
+        }
+    }
+}
